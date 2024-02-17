f46ee65ab87271e9d5c40626906742b2cc0f35fc
ishita-lyall <55630536+ishita-lyall@users.noreply.github.com>
2024-02-17 23:50:52 +0530

    Update ReverseLinkedList.java

--- linkedList/reverseLinkedList.java
+++ linkedList/reverseLinkedList.java
@@ -1,1 +1,16 @@
+class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode nextNode;
 
+        while (current != null) {
+            nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+}
