adacd6625afa54146ad8643a014d581deffd0cb6
ishita-lyall <55630536+ishita-lyall@users.noreply.github.com>
2024-02-27 01:00:53 +0530

    Create FindDuplicate.java

---
+++ arrays/FindDuplicate.java
@@ -0,0 +1,14 @@
+class FindDuplicate {
+    public int findDuplicate(int[] nums) {
+    HashSet<Integer> set = new HashSet<>();
+
+    for (int num : nums) {
+        if (set.contains(num)) {
+                return num;
+            } else {
+                set.add(num);
+            }
+        }
+        return -1;
+    }
+}
